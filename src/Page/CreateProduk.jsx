8905c44a7567b3e056b6d0fba0f75665c5e021b6
dewiAnjani11 <da0189772@.gmail.com>
2024-01-13 18:15:19 +0700

    pembuatan user

--- src/Page/CreateProduk.jsx
+++ src/Page/CreateProduk.jsx
@@ -18,7 +18,7 @@ export default function CreateProdukPage() {
     const { register, handleSubmit } = useForm();
     const onSubmit = async (data) => {
         try {
-            const baseURL = 'http://localhost:1337/api/testings';
+            const baseURL = 'http://localhost:1337/api/produks';
             const response = await axios.post(baseURL, data);
             console.log(response.data);
         } catch (error) {
@@ -49,55 +49,55 @@ export default function CreateProdukPage() {
                 </div> */}
                 <div>
                     <label className="form-label">Nama Produk</label>
-                    <input {...register("nama")} type="text" className="form-control" />
+                    <input {...register("nama",{required:true})} type="text" className="form-control" />
+                </div>
+                <div className="mt-3">
+                    <label className="form-label">Deskripsi</label>
+                    <textarea {...register("deskripsi",{required:true})} className="form-control"></textarea>
                 </div>
                 <div className="mt-3">
                     <label className="form-label">Harga</label>
-                    <input {...register("harga")} type="number" className="form-control" />
+                    <input {...register("harga",{required:true})} type="number" className="form-control" />
                 </div>
                 <div className="mt-3">
                     <label className="form-label">Brand</label>
-                    <input {...register("brand")} type="text" className="form-control" />
-                </div>
-                <div className="mt-3">
-                    <label className="form-label">Nama Toko</label>
-                    <input {...register("nama_toko")} type="text" className="form-control" />
+                    <input {...register("brand",{required:true})} type="text" className="form-control" />
                 </div>
                 <div className="mt-3">
                     <label className="form-label">Terjual</label>
-                    <input {...register("terjual")} type="number" className="form-control" />
+                    <input {...register("terjual",{required:true})} type="number" className="form-control" />
+                </div>
+                <div className="mt-3">
+                    <label className="form-label">Rating</label>
+                    <input {...register("rating",{required:true})} type="number" className="form-control" />
+                </div>
+                <div className="mt-3">
+                    <label className="form-label">Stock</label>
+                    <input {...register("stock",{required:true})} type="number" className="form-control" />
+                </div>
+                <div className="mt-3">
+                    <label className="form-label">minimal pembelian</label>
+                    <input {...register("minPembelian",{required:true})} type="number" className="form-control" />
+                </div>
+                <div className="mt-3">
+                    <label className="form-label">Nama Toko</label>
+                    <input {...register("nama_toko",{required:true})} type="text" className="form-control" />
                 </div>
                 <div className="mt-3">
                     <label className="form-label">Kategori</label>
-                    <select className="form-select" {...register("kategori")}>
+                    <select className="form-control"{...register("kategori",{required:true})}>
                         {categories.map((item, index) => (
                             <option value={item.id} key={index}>{item.id}</option>
                         ))}
                     </select>
                 </div>
                 <div className="mt-3">
-                    <label className="form-label">Rating</label>
-                    <input {...register("rating")} type="number" className="form-control" />
-                </div>
-                <div className="mt-3">
-                    <label className="form-label">Stock</label>
-                    <input {...register("stock")} type="number" className="form-control" />
-                </div>
-                <div className="mt-3">
-                    <label className="form-label">minimal pembelian</label>
-                    <input {...register("minPembelian")} type="number" className="form-control" />
-                </div>
-                <div className="mt-3">
                     <label className="form-label">Promo presentasi</label>
-                    <input {...register("promoPercentage")} type="number" className="form-control" />
+                    <input {...register("promoPercentage",{required:true})} type="number" className="form-control" />
                 </div>
                 <div className="mt-3">
                     <label className="form-label">Berat</label>
-                    <input {...register("berat")} type="number" className="form-control" />
-                </div>
-                <div className="mt-3">
-                    <label className="form-label">Deskripsi</label>
-                    <textarea {...register("deskripsi")} className="form-control"></textarea>
+                    <input {...register("berat",{required:true})} type="number" className="form-control" />
                 </div>
                 <button type="submit" className="btn btn-primary my-4">Tambah</button>
             </form>
